c132d07199f0532580db219cb90e661f704e2174
99002559 <71485099+99002559@users.noreply.github.com>
2020-11-09 20:03:28 +0530

    Add init function for CubeGUI class

--- CubeGUI.py
+++ CubeGUI.py
@@ -1,1 +1,20 @@
+import pygame
+import time
+pygame.font.init()
+
 class CubeGUI:
+    faces = cube
+        # colors
+        white = [255, 255, 255]
+        green = [0, 255, 0]
+        blue = [0, 0, 255]
+        orange = [255, 165, 0]
+        yellow = [255, 255, 0]
+        red = [255, 0, 0]
+
+        def __init__(self, rows, cols, width, height, win):
+            self.rows = rows
+            self.cols = cols
+            self.width = width
+            self.height = height
+            self.win = win
