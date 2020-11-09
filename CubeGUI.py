959c34d7dd6cbe5ab79e2ea9653de5f1ae97da72
99002649 <71537287+99002649@users.noreply.github.com>
2020-11-09 20:04:21 +0530

    Update CubeGUI.py

--- CubeGUI.py
+++ CubeGUI.py
@@ -18,3 +18,18 @@ class CubeGUI:
             self.width = width
             self.height = height
             self.win = win
+        def grid(self):
+            for i in range(self.rows+1):
+                if(i % 3 == 0):
+                    pygame.draw.line(self.win, (0, 0, 0), (0, 0 +
+                                                           i*self.width/3), (1200, i*self.width/3), 4)
+                else:
+                    pygame.draw.line(self.win, (0, 0, 0), (0, 0 +
+                                                           i*self.width/3), (1200, i*self.width/3), 2)
+            for i in range(self.cols+1):
+                if(i % 3 == 0):
+                    pygame.draw.line(self.win, (0, 0, 0), (0 + i *
+                                                           self.width/3, 0), (0 + i*self.width/3, 900), 4)
+                else:
+                    pygame.draw.line(self.win, (0, 0, 0), (0 + i *
+                                                           self.width/3, 0), (0 + i*self.width/3, 900), 2)
