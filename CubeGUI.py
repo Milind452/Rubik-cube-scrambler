4e83c74c2ee0d54a779644f2cbe63772c51f2a90
Milind Mohapatra <milind.mohapatra@ltts.com>
2020-11-09 22:22:01 +0530

    Fix indentation

--- CubeGUI.py
+++ CubeGUI.py
@@ -10,73 +10,73 @@ class CubeGUI:
     green = [0, 255, 0]
     blue = [0, 0, 255]
     orange = [255, 165, 0]
-     yellow = [255, 255, 0]
-      red = [255, 0, 0]
+    yellow = [255, 255, 0]
+    red = [255, 0, 0]
 
-       def __init__(self, rows, cols, width, height, win):
-            self.rows = rows
-            self.cols = cols
-            self.width = width
-            self.height = height
-            self.win = win
+    def __init__(self, rows, cols, width, height, win):
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
+        self.win = win
 
-        def grid(self):
-            for i in range(self.rows+1):
-                if(i % 3 == 0):
-                    pygame.draw.line(self.win, (0, 0, 0), (0, 0 +
-                                                           i*self.width/3), (1200, i*self.width/3), 4)
-                else:
-                    pygame.draw.line(self.win, (0, 0, 0), (0, 0 +
-                                                           i*self.width/3), (1200, i*self.width/3), 2)
-            for i in range(self.cols+1):
-                if(i % 3 == 0):
-                    pygame.draw.line(self.win, (0, 0, 0), (0 + i *
-                                                           self.width/3, 0), (0 + i*self.width/3, 900), 4)
-                else:
-                    pygame.draw.line(self.win, (0, 0, 0), (0 + i *
-                                                           self.width/3, 0), (0 + i*self.width/3, 900), 2)
+    def grid(self):
+        for i in range(self.rows+1):
+            if(i % 3 == 0):
+                pygame.draw.line(self.win, (0, 0, 0), (0, 0 +
+                                                        i*self.width/3), (1200, i*self.width/3), 4)
+            else:
+                pygame.draw.line(self.win, (0, 0, 0), (0, 0 +
+                                                        i*self.width/3), (1200, i*self.width/3), 2)
+        for i in range(self.cols+1):
+            if(i % 3 == 0):
+                pygame.draw.line(self.win, (0, 0, 0), (0 + i *
+                                                        self.width/3, 0), (0 + i*self.width/3, 900), 4)
+            else:
+                pygame.draw.line(self.win, (0, 0, 0), (0 + i *
+                                                        self.width/3, 0), (0 + i*self.width/3, 900), 2)
 
 
-        def drawFace(self, face, gap):
+    def drawFace(self, face, gap):
+        x = 1
+        y = 0
+        if(face == 0):
             x = 1
             y = 0
-            if(face == 0):
-                x = 1
-                y = 0
-            elif(face == 5):
-                x = 1
-                y = 600
-            else:
-                x = face - 1
-                y = 300
+        elif(face == 5):
+            x = 1
+            y = 600
+        else:
+            x = face - 1
+            y = 300
 
-            for i in range(0, 3):
-                for j in range(0, 3):
-                    if(self.faces[face][i][j] == 'w'):
-                        pygame.draw.rect(self.win, self.white, [
-                            j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if(self.faces[face][i][j] == 'w'):
+                    pygame.draw.rect(self.win, self.white, [
+                        j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
 
-                    if(self.faces[face][i][j] == 'g'):
-                        pygame.draw.rect(self.win, self.green, [
-                            j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
+                if(self.faces[face][i][j] == 'g'):
+                    pygame.draw.rect(self.win, self.green, [
+                        j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
 
-                    if(self.faces[face][i][j] == 'b'):
-                        pygame.draw.rect(self.win, self.blue, [
-                            j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
+                if(self.faces[face][i][j] == 'b'):
+                    pygame.draw.rect(self.win, self.blue, [
+                        j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
 
-                    if(self.faces[face][i][j] == 'o'):
-                        pygame.draw.rect(self.win, self.orange, [
-                            j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
+                if(self.faces[face][i][j] == 'o'):
+                    pygame.draw.rect(self.win, self.orange, [
+                        j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
 
-                    if(self.faces[face][i][j] == 'r'):
-                        pygame.draw.rect(self.win, self.red, [
-                            j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
+                if(self.faces[face][i][j] == 'r'):
+                    pygame.draw.rect(self.win, self.red, [
+                        j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
 
-                    if(self.faces[face][i][j] == 'y'):
-                        pygame.draw.rect(self.win, self.yellow, [
-                            j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
+                if(self.faces[face][i][j] == 'y'):
+                    pygame.draw.rect(self.win, self.yellow, [
+                        j*gap+300*x,  i*gap+y, self.width/3, self.height/3])
 
-        def draw(self):
+    def draw(self):
         self.win.fill((0, 0, 0))
         gap = self.width/3
         for face in range(len(self.faces)):
